7fcfac104bb5a00559a7cb041d2084c95eed63ee
Fabian Segatz <fabian.segatz@gmail.com>
2022-03-29 09:20:26 +0200

    test7

--- demos/include/dd2482_demo.h
+++ demos/include/dd2482_demo.h
@@ -4,7 +4,7 @@
 #include "aws_demo_config.h"
 
 // MEANING OF LIFE
-#define MEANING_OF_LIFE     "42"
+#define MEANING_OF_LIFE     "41"
 
 // IotThreadRoutine_t runTestDemo ()
 void runTestDemo( void * pArgument );
